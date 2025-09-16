d0b22a3669f4bd477d839a9fa3f676d967569c4b
Brayan Neves <838126+nevesb@users.noreply.github.com>
2025-09-16 20:36:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/zenys-refiner/", // Base path para GitHub Pages
+  base: "/romc-refine/", // Base path para GitHub Pages
   server: {
     host: "::",
     port: 8080,
